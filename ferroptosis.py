9c46e0d273870c37ed1b4f63cd2b09b0b81cbff3
lh64 <lh64@cornell.edu>
2025-01-08 21:06:29 -0600

    Made some small superficial changes to the code

--- ferroptosis.py
+++ ferroptosis.py
@@ -4,7 +4,6 @@ import numpy as np
 import matplotlib.pyplot as plt
 from scipy.constants import N_A, pi
 
-# test commit
 Model()
 
 # Volume
@@ -12,10 +11,14 @@ Diameter = 15  # micrometers https://www.ncbi.nlm.nih.gov/pmc/articles/PMC407900
 Volume = 4/3 * pi * (Diameter / 1e6 / 2)**3  # m^3
 
 # Metabolites
-Monomer("Glu_intra")
-Monomer("Cystine_extra")
-Monomer("Glu_extra")
-Monomer("Cystine_intra")
+# Monomer("Glu_intra")
+# Monomer("Cystine_extra")
+# Monomer("Glu_extra")
+# Monomer("Cystine_intra")
+
+Monomer('Glu', ['loc'], {'loc': ['intra', 'extra']})
+Monomer('Cystine', ['loc'], {'loc': ['intra', 'extra']})
+
 Monomer("Cys")
 Monomer("Glu_Cys_GCL_Product")
 Monomer("Gly")
@@ -87,8 +90,10 @@ Initial(GCL(), GCL_0)
 Initial(GSS(), GSS_0)
 Initial(Gly(), Gly_0)
 # initials above included in StartSystemxc_EndGSH.py
-Initial(Glu_intra(), Glu_intra_0)
-Initial(Cystine_extra(), Cystine_extra_0)
+# Initial(Glu_intra(), Glu_intra_0)
+Initial(Glu(loc='intra'), Glu_intra_0)
+# Initial(Cystine_extra(), Cystine_extra_0)
+Initial(Cystine(loc='extra'), Cystine_extra_0)
 Initial(Erastin(sys_xc=None), Erastin_0)
 Initial(RSL3(gpx4=None), RSL3_0)
 Initial(GPX4(rsl3=None), GPX4_0)
@@ -112,10 +117,10 @@ Initial(LOH(), LOH_0)
 
 # Observables
 Observable("Cys_Obs", Cys())
-Observable("Cystine_extra_Obs", Cystine_extra())
-Observable("Cystine_intra_Obs", Cystine_intra())
-Observable("Glu_intra_Obs", Glu_intra())
-Observable("Glu_extra_Obs", Glu_extra())
+Observable("Cystine_extra_Obs", Cystine(loc='extra')) # Cystine_extra())
+Observable("Cystine_intra_Obs", Cystine(loc='intra')) # Cystine_intra())
+Observable("Glu_intra_Obs", Glu(loc='intra')) # Glu_intra())
+Observable("Glu_extra_Obs", Glu(loc='extra')) # Glu_extra())
 Observable("Gly_Obs", Gly())
 Observable("GSH_Obs", GSH())
 Observable("Glu_Cys_GCL_Product_Obs", Glu_Cys_GCL_Product())
@@ -131,48 +136,57 @@ Observable("NADPplus_Obs", NADPplus())
 Observable("NADPtotal", NADPH()+NADPplus())
 Observable("Irontotal", Iron())
 
-
 # Rules
 # Glu (intracellular) + Cystine (extracellular) + System Xc-->
 # Glu (extracellular) + Cystine (intracellular) + System Xc-
 
 Parameter("k_Glu_intra", 2e3)
-Rule("Glu_Intra_Synthesis", None >> Glu_intra(), k_Glu_intra)
+# Rule("Glu_Intra_Synthesis", None >> Glu_intra(), k_Glu_intra)
+Rule("Glu_Intra_Synthesis", None >> Glu(loc='intra'), k_Glu_intra)
 
 Parameter("k_Glu_extra", 0.1)  # 5 originally
-Rule("Glu_Extra_Degradation", Glu_extra() >> None, k_Glu_extra)
+# Rule("Glu_Extra_Degradation", Glu_extra() >> None, k_Glu_extra)
+Rule("Glu_Extra_Degradation", Glu(loc='extra') >> None, k_Glu_extra)
 
 Parameter("k_Cystine_extra", 1e3)
-Rule("Cystine_Extra_Synthesis", None >> Cystine_extra(), k_Cystine_extra)
+# Rule("Cystine_Extra_Synthesis", None >> Cystine_extra(), k_Cystine_extra)
+Rule("Cystine_Extra_Synthesis", None >> Cystine(loc='extra'), k_Cystine_extra)
 
 Parameter("kcat_Glu_Cystine", 1e-7)
 # Parameter("km_Glu_Cystine", 1e7)
 # Expression("keff_Glu_Cystine", kcat_Glu_Cystine / (km_Glu_Cystine+Glu_intra_Obs+Cystine_extra_Obs))
-Rule("Glu_Cystine_Transport", Glu_intra() + Cystine_extra() + System_Xc(erastin=None) >>
-     Glu_extra() + Cystine_intra() + System_Xc(erastin=None), kcat_Glu_Cystine)
+# Rule("Glu_Cystine_Transport",
+#      Glu_intra() + Cystine_extra() + System_Xc(erastin=None) >>
+#      Glu_extra() + Cystine_intra() + System_Xc(erastin=None), kcat_Glu_Cystine)
+Rule("Glu_Cystine_Transport",
+     Glu(loc='intra') + Cystine(loc='extra') + System_Xc(erastin=None) >>
+     Glu(loc='extra') + Cystine(loc='intra') + System_Xc(erastin=None), kcat_Glu_Cystine)
 
 # Cystine (intracellular)  -> Cys
 Parameter("k_Cystine_intra", 0.1)
-Rule("Cystine_intra_to_cys", Cystine_intra() >> Cys(), k_Cystine_intra)
+# Rule("Cystine_intra_to_cys", Cystine_intra() >> Cys(), k_Cystine_intra)
+Rule("Cystine_intra_to_cys", Cystine(loc='intra') >> Cys(), k_Cystine_intra)
 
 # Cys  + Trx/TrxR -> ? + Trx/TrxR  # Note: Giving value here is important to stabilize
 Parameter("kcat_Cys_TrxR", 0)
 # Parameter("km_Cys_TrxR", 100)
 # Expression("keff_Cys_TrxR", kcat_Cys_TrxR / (km_Cys_TrxR+Cys_Obs))
-Rule("Cys_TrxR", Cys() + Trx_TrxR() >> Trx_TrxR(), kcat_Cys_TrxR)
+Rule("Cys_deg_TrxR", Cys() + Trx_TrxR() >> Trx_TrxR(), kcat_Cys_TrxR)
 
 # Glu + Cys + GCL -> Glu_Cys_GCL_Product + GCL
 Parameter("kcat_Glu_Cys_GCL", 2e-7)
 # Parameter("km_Glu_Cys_GCL", 1e7) # 100
 # Expression("keff_Glu_Cys_GCL", kcat_Glu_Cys_GCL / (km_Glu_Cys_GCL+Cys_Obs+Glu_intra_Obs))
-Rule("Glu_Cys_GCL", Glu_intra() + Cys() + GCL() >> Glu_Cys_GCL_Product() + GCL(), kcat_Glu_Cys_GCL)
+# Rule("Glu_Cys_GCL", Glu_intra() + Cys() + GCL() >> Glu_Cys_GCL_Product() + GCL(), kcat_Glu_Cys_GCL)
+Rule("Glu_Cys_GCL", Glu(loc='intra') + Cys() + GCL() >> Glu_Cys_GCL_Product() + GCL(), kcat_Glu_Cys_GCL)
 
 # Glu_Cys_GCL_Product + Gly + GSS -> GSH + GSS
 Parameter("kcat_Gly_Glu_Cys_GCL_Product", 7e-5)  # 2e-7, 1e-4
 Parameter("km_Gly_Glu_Cys_GCL_Product", 100)  # 100
 Expression("keff_Gly_Glu_Cys_GCL_Product",
            kcat_Gly_Glu_Cys_GCL_Product / (km_Gly_Glu_Cys_GCL_Product+Gly_Obs+Glu_Cys_GCL_Product_Obs))
-Rule("Gly_Glu_Cys_GCL_Product", Glu_Cys_GCL_Product() + Gly() + GSS() >> GSH() + GSS(), keff_Gly_Glu_Cys_GCL_Product)
+Rule("Gly_Glu_Cys_GCL_Product", Glu_Cys_GCL_Product() + Gly() + GSS() >> GSH() + GSS(),
+     keff_Gly_Glu_Cys_GCL_Product)
 
 Parameter("k_Gly", 20)  # 100
 Rule("Gly_Synthesis", None >> Gly(), k_Gly)
@@ -263,108 +277,97 @@ Parameter("kr_Chelators", 100)  # 1
 Rule("Chelators_Iron", Iron_chelators(iron=None) + Iron(b=None) | Iron(b=1) % Iron_chelators(iron=1),
      kf_Chelators, kr_Chelators)
 
-# plotting code
-Tspan = np.linspace(0, 200, 1001)
-print(Tspan)
+if __name__ == '__main__':
 
-for km in [100]:
-    sim = ScipyOdeSimulator(model, Tspan, verbose=True)
-    result = sim.run()  # ()(param_values={"km_LOOH_GSH": km})
+    # run simulation
+    Tspan = np.linspace(0, 200, 1001)
+    sim = ScipyOdeSimulator(model, Tspan, verbose=False)
 
-    obs2plot = [["Cystine_extra_Obs", "Cystine_intra_Obs", "Cys_Obs"],
-                ["Glu_intra_Obs", "Glu_extra_Obs"],
-                ["Gly_Obs","GSSG_Obs" ,"GSH_Obs"],
-                ["LOOH_Obs", "LOH_Obs"],
-                ["NADPH_Obs","NADPplus_Obs","NADPtotal"],
-                ["Irontotal","Lipid_metab_Obs","LO_Obs"]]
+    for km in [100, 1e3, 1e4]:
+        print('km_LOOH_GSH:', km)
+        result = sim.run(param_values={"km_LOOH_GSH": km})
 
-    # obs2plot = [["Cystine_extra_Obs", "Cystine_intra_Obs", "Cys_Obs"],
-    #             ["Glu_intra_Obs", "Glu_extra_Obs"],
-    #             ["Gly_Obs", "GSH_Obs", "GSSG_Obs"],
-    #             ["NADPplus_Obs", "NADPH_Obs"]]
+        # plot results
+        obs2plot = [["Cystine_extra_Obs", "Cystine_intra_Obs", "Cys_Obs"],
+                    ["Glu_intra_Obs", "Glu_extra_Obs"],
+                    ["Gly_Obs","GSSG_Obs" ,"GSH_Obs"],
+                    ["LOOH_Obs", "LOH_Obs"],
+                    ["NADPH_Obs","NADPplus_Obs","NADPtotal"],
+                    ["Irontotal","Lipid_metab_Obs","LO_Obs"]]
 
-    fig, axs = plt.subplots(nrows=len(obs2plot), ncols=1, sharex=True, constrained_layout = True,
-                            figsize = (9.6, 2.4*len(obs2plot)))
-    # default figsize = (6.4,4.8)
-    # print(len(axs))
-    # quit()
-    fig.suptitle("km_LOOH_GSH = %g" % km, fontsize=15)
-    fig.supxlabel("Time")
-    fig.supylabel("Concentration")
+        fig, axs = plt.subplots(nrows=len(obs2plot), ncols=1, sharex='all', constrained_layout=True,
+                                figsize = (9.6, 2.4*len(obs2plot)))
+        # default figsize = (6.4,4.8)
+        fig.suptitle("km_LOOH_GSH = %g" % km, fontsize=16)
+        fig.supxlabel("Time", fontsize=16)
+        fig.supylabel("Concentration", fontsize=16)
 
-    row = 0
-    col = 0
-    for obs_group in obs2plot:
-        for obs_name in obs_group:
+        row = 0
+        col = 0
+        for obs_group in obs2plot:
+            for obs_name in obs_group:
+                axs[row].plot(Tspan, result.observables[obs_name], lw=2, label=obs_name)
+                axs[row].legend(loc="best", fontsize=16)
+                axs[row].xaxis.set_tick_params(labelsize=16)
+                axs[row].yaxis.set_tick_params(labelsize=16)
+            if (col+1) % 1 == 0:
+                row += 1
+                col = 0
+            else:
+                col += 1
 
-            axs[row].plot(Tspan, result.observables[obs_name], lw=2, label=obs_name)
-            #if row == 1:
-               # axs[row].set_xlabel("Time", fontsize=16)
-           # axs[row].set_ylabel("Concentration", fontsize=16)
-            axs[row].legend(loc="best", fontsize=16)
-            axs[row].xaxis.set_tick_params(labelsize=16)
-            axs[row].yaxis.set_tick_params(labelsize=16)
+        # plt.savefig("km_Glu_Cys_%g.pdf" % km,format="pdf")
 
-        if (col+1) % 1 == 0:
-            row += 1
-            col = 0
-        else:
-            col += 1
+    plt.show()
 
-    # plt.tight_layout()
-    # plt.savefig("km_Glu_Cys_%g.pdf" % km,format="pdf")
-
-plt.show()
-
-
-# Tspan=np.linspace(0,1000,10001)
-# print(Tspan)
-#
-# for km in [1e5,1e7,1e8,1e9]:
-#     sim=ScipyOdeSimulator(model,Tspan,verbose=True)
-#     result=sim.run(param_values={"km_LOOH_GSH": km})
-#
-#     #obs2plot=["LOOH_Obs","LOH_Obs","LO_Obs"]
-#     obs2plot=[["LO_Obs"],
-#               ["LOOH_Obs","LOH_Obs"],
-#               ["GSH_Obs","GSSG_Obs"],
-#               ["NADPplus_Obs","NADPH_Obs"],
-#               ["Cystine_extra_Obs","Cystine_intra_Obs","Cys_Obs"],
-#               ["Glu_intra_Obs"]]#,"Glu_extra_Obs"]]
-#
-#     fig,axs=plt.subplots(nrows=3,ncols=2,sharex=True,figsize=(9.6,9.6))
-#     fig.suptitle("km_LOOH_GSH = %g" % km, fontsize=15)
-#
-#     #use this code for plotting multiple km values
-#     #fig.suptitle("km_Glu_Cystine = %g,\n km_Glu_Cystine = %g,\n km_Glu_Cystine = %g" %
-#                  #(km_Glu_Cystine.value,km_Glu_Cystine.value,km_Glu_Cystine.value), fontsize=15)
-#
-#     row=0
-#     col=0
-#     for obs_group in obs2plot:
-#         #plt.figure()
-#         for obs_name in obs_group:
-#             #model.observables:
-#             #plt.figure()
-#             maxconc=LOOH_0.value
-#             #plt.plot(Tspan,result.observables[obs_name]/maxconc,lw=2,label=obs_name)
-#             axs[row,col].plot(Tspan, result.observables[obs_name], lw=2, label=obs_name)
-#             if row == 2:
-#                 axs[row,col].set_xlabel("Time")
-#             axs[row,col].set_ylabel("Concentration")
-#             axs[row,col].legend(loc="best")
-#
-#         if (col+1) % 2 == 0:
-#             row += 1
-#             col = 0
-#         else:
-#             col += 1
-#
-#             plt.tight_layout()
-#     plt.savefig("km_Glu_Cys_%g.pdf" % km,format="pdf")
-#
-#
-#
-# #Use this code if planning to save files with multiple km changes
-# #plt.savefig("km_1_%g,km_2_%g,km_3_%g.pdf" % (km_1,km_2,km_3),format="pdf")
-# plt.show()
+    # Tspan=np.linspace(0,1000,10001)
+    # print(Tspan)
+    #
+    # for km in [1e5,1e7,1e8,1e9]:
+    #     sim=ScipyOdeSimulator(model,Tspan,verbose=True)
+    #     result=sim.run(param_values={"km_LOOH_GSH": km})
+    #
+    #     #obs2plot=["LOOH_Obs","LOH_Obs","LO_Obs"]
+    #     obs2plot=[["LO_Obs"],
+    #               ["LOOH_Obs","LOH_Obs"],
+    #               ["GSH_Obs","GSSG_Obs"],
+    #               ["NADPplus_Obs","NADPH_Obs"],
+    #               ["Cystine_extra_Obs","Cystine_intra_Obs","Cys_Obs"],
+    #               ["Glu_intra_Obs"]]#,"Glu_extra_Obs"]]
+    #
+    #     fig,axs=plt.subplots(nrows=3,ncols=2,sharex=True,figsize=(9.6,9.6))
+    #     fig.suptitle("km_LOOH_GSH = %g" % km, fontsize=15)
+    #
+    #     #use this code for plotting multiple km values
+    #     #fig.suptitle("km_Glu_Cystine = %g,\n km_Glu_Cystine = %g,\n km_Glu_Cystine = %g" %
+    #                  #(km_Glu_Cystine.value,km_Glu_Cystine.value,km_Glu_Cystine.value), fontsize=15)
+    #
+    #     row=0
+    #     col=0
+    #     for obs_group in obs2plot:
+    #         #plt.figure()
+    #         for obs_name in obs_group:
+    #             #model.observables:
+    #             #plt.figure()
+    #             maxconc=LOOH_0.value
+    #             #plt.plot(Tspan,result.observables[obs_name]/maxconc,lw=2,label=obs_name)
+    #             axs[row,col].plot(Tspan, result.observables[obs_name], lw=2, label=obs_name)
+    #             if row == 2:
+    #                 axs[row,col].set_xlabel("Time")
+    #             axs[row,col].set_ylabel("Concentration")
+    #             axs[row,col].legend(loc="best")
+    #
+    #         if (col+1) % 2 == 0:
+    #             row += 1
+    #             col = 0
+    #         else:
+    #             col += 1
+    #
+    #             plt.tight_layout()
+    #     plt.savefig("km_Glu_Cys_%g.pdf" % km,format="pdf")
+    #
+    #
+    #
+    # #Use this code if planning to save files with multiple km changes
+    # #plt.savefig("km_1_%g,km_2_%g,km_3_%g.pdf" % (km_1,km_2,km_3),format="pdf")
+    # plt.show()
