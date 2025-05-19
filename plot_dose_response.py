e2fc4679a03b1363564f4f43867a5d50d90937d4
lh64 <lh64@cornell.edu>
2025-05-19 01:42:15 -0500

    Small change to plot_dose_response.py

--- plot_dose_response.py
+++ plot_dose_response.py
@@ -4,9 +4,11 @@ basepath = 'RESULTS'
 directories = ['HT1080', 'U2OS']
 run_pydream_filename = 'run_ferroptosis_pydream.py'
 expts_doses = [
+    # experiment 0
     {'ht1080_erastin': [0, 0.056755444, 0.113703658, 0.228802973, 0.455863303],
       'xlabel': r'Erastin ($\mu$M)',
       'title': 'HT-1080'},
+    # experiment 1
     {'u2os_erastin': [0, 0.216165686, 0.441596578, 0.896487612, 1.80157234],
      'xlabel': r'Erastin ($\mu$M)',
      'title': 'U2-OS'}
